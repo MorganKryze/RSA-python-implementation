51e28e57c1db11832107995effb5d04f546555ed
Morgan <103436411+MorganKryze@users.noreply.github.com>
2023-12-20 16:08:39 +0100

    🚑 (py) update functions to safer ones

--- src/demo.py
+++ src/demo.py
@@ -4,64 +4,7 @@ import hashlib as hs
 
 import gmpy2
 
-
-def my_gcd(a: int, b: int) -> int:
-    """Calculate the greatest common divisor of two numbers.
-
-    Args:
-    ----
-    a (int): The first number.
-    b (int): The second number.
-
-    Returns:
-    -------
-    int: The greatest common divisor of the two numbers.
-    """
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-def my_invert(a: int, b: int) -> int | None:
-    """Calculate the inverse of a mod b.
-
-    Args:
-    ----
-    a (int): The number to be inverted.
-    b (int): The modulus.
-
-    Returns:
-    -------
-    int: The inverse of a mod b.
-    """
-    if my_gcd(a, b) != 1:
-        return None
-    u1, u2, u3 = 1, 0, a
-    v1, v2, v3 = 0, 1, b
-    while v3 != 0:
-        q = u3 // v3
-        v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
-    return u1 % b
-
-def my_powmod(a: int, b: int, c: int) -> int:
-    """Calculate a^b mod c.
-
-    Args:
-    ----
-    a (int): The base.
-    b (int): The exponent.
-    c (int): The modulus.
-
-    Returns:
-    -------
-    int: a^b mod c.
-    """
-    result = 1
-    while b > 0:
-        if b & 1:
-            result = (result * a) % c
-        b >>= 1
-        a = (a * a) % c
-    return result
+KEY_SIZE = 2048
 
 def generate_prime() -> int:
     """Generate a random prime number of size KEY_SIZE/2.
@@ -84,8 +27,8 @@ def key_gen() -> tuple:
         e = gmpy2.mpz_random( rand, phi)
     assert (e > 1)
     assert (e < phi)
-    assert (my_gcd(e, phi) == 1)
-    d = my_invert(e, phi)
+    assert (gmpy2.gcd(e, phi) == 1)
+    d = gmpy2.invert(e, phi)
     assert (d != 1)
     assert (gmpy2.t_mod(gmpy2.mul(e, d), phi) == 1)
     return (n, e, d)
@@ -105,7 +48,7 @@ def encrypt(n: int, e: int, m: int) -> int:
     -------
     int: The encrypted message.
     """
-    return my_powmod(m, e, n)
+    return gmpy2.powmod(m, e, n)
 
 def decrypt(n: int, d: int, c: int) -> int:
     """Decrypt a message using RSA decryption.
@@ -120,7 +63,7 @@ def decrypt(n: int, d: int, c: int) -> int:
     -------
     int: The decrypted message.
     """
-    return my_powmod(c, d, n)
+    return gmpy2.powmod(c, d, n)
 
 
 def sign(n: int, d: int, m: int | str) -> int:
@@ -137,7 +80,7 @@ def sign(n: int, d: int, m: int | str) -> int:
     int: The signature of the message.
     """
     hashed = int(hs.sha256(str(m).encode("utf-8")).hexdigest(), 16)
-    return my_powmod(hashed, d, n)
+    return gmpy2.powmod(hashed, d, n)
 
 def verify(n: int, e: int, m: int | str, s: int | str) -> bool:
     """Verify a message using RSA signature.
@@ -154,13 +97,12 @@ def verify(n: int, e: int, m: int | str, s: int | str) -> bool:
     bool: True if the signature is valid, False otherwise.
     """
     hashed = int(hs.sha256(str(m).encode("utf-8")).hexdigest(), 16)
-    return gmpy2.mod(hashed, n) == my_powmod(s, e, n)
+    return gmpy2.mod(hashed, n) == gmpy2.powmod(s, e, n)
 
 
 
 if __name__ == "__main__":
-    KEY_SIZE = 2048
-    rand = gmpy2. random_state()
+    rand = gmpy2.random_state()
 
     message = gmpy2.mpz_urandomb(rand, 16)
     long_message = gmpy2.mpz_urandomb(rand, 2048)
